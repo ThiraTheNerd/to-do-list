b723b7488efdee59f57fbd6e6f059e56b97c98d6
ThiraTheNerd <thiragithinji@gmail.com>
2021-03-01 21:12:40 +0300

    add functionality to record new tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
+
+  function Activity(name){
+    this.activityName = name;
+  }
+
 $(document).ready(function(){
-  $("form#my-todo").submit(function(){
+  $("form#my-todo").submit(function(event){
+    event.preventDefault();
 
-    var task = $("input#activity").val();
-    
+    var inputtedTask = $("input#activity").val();
 
-  });
+    var newTask = new Activity(inputtedTask);
+
+    $("p#no-task").hide();
+    $("ul#my-tasks").append("<li><span class = 'new-task'>" +newTask.activityName+ "</span></li>");
+
+    $("input#activity").val("");
+
+});
 });
